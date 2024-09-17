45b12f6c95502406cba46e8182abb952ec7e4c34
AGaelleB <annegaelle.bonnefoy@gmail.com>
2024-09-17 11:36:19 +0200

    html show helloword

--- django/mybackend/settings.py
+++ django/mybackend/settings.py
@@ -9,6 +9,6 @@ DATABASES = {
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': os.getenv('POSTGRES_HOST'),
-        'PORT': os.getenv('POSTGRES_PORT'),        
+        'PORT': os.getenv('POSTGRES_PORT'),      
     }
 }
