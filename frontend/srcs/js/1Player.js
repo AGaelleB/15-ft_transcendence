883cb8c38abb6928f9636e7f35fccb6c4588cc93
Baptiste <b.fresquet06@gmail.com>
2024-09-20 15:28:09 +0200

    2 modals bisbis

--- frontend/srcs/js/1Player.js
+++ frontend/srcs/js/1Player.js
@@ -24,7 +24,7 @@ document.addEventListener('DOMContentLoaded', function() {
     closeSettingsButton.addEventListener('click', () => {
         settingsModal.style.display = 'none';
     });
-    
+
     let gameStarted = false;
 
     function startGame() {
@@ -123,7 +123,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function showWinMessage(winner) {
         const winnerMessage = document.querySelector('.message');
-        winnerMessage.innerHTML = `Player ${winner} Wins!`;
+        winnerMessage.innerHTML = `Player ${winner} Wins! <i class="bi bi-emoji-sunglasses"></i>`;
 
         const modal = document.querySelector('.modal');
         modal.style.display = 'block';
