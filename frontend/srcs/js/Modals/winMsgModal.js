65ae2e7013615c178dbbae3c3a712322cfa07fce
AGaelleB <annegaelle.bonnefoy@gmail.com>
2024-09-25 15:36:02 +0200

    winmsgmodal

--- frontend/srcs/js/Modals/winMsgModal.js
+++ frontend/srcs/js/Modals/winMsgModal.js
@@ -1,2 +1,9 @@
 // frontend/srcs/js/Modals/winMsgModal.js
 
+export function showWinMessage(winner) {
+    const winnerMessage = document.querySelector('.message');
+    winnerMessage.innerHTML = `Player ${winner} Wins! <i class="bi bi-emoji-sunglasses"></i>`;
+
+    const modal = document.querySelector('.modal');
+    modal.style.display = 'block';
+}
