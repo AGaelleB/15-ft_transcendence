9031afcf82e3d8282aac757ff0729dd40182e0e2
AGaelleB <annegaelle.bonnefoy@gmail.com>
2024-09-26 10:14:59 +0200

    to do maj

--- frontend/srcs/js/PongGame/computerIA.js
+++ frontend/srcs/js/PongGame/computerIA.js
@@ -1,5 +1,17 @@
 // frontend/srcs/js/PongGame/computerIA.js
 
+/* 
+    - A algorithm interdit* : Ne pas utiliser l'algorithme A*.
+    - Comportement humain : L'IA doit simuler des actions via des entrées clavier.
+    - Rafraîchissement toutes les secondes : L'IA ne peut rafraîchir sa vue qu'une fois par seconde.
+    - Anticipation : L'IA doit anticiper les rebonds et les actions.
+    - Utilisation des power-ups : Si des power-ups sont implémentés, l'IA doit les utiliser.
+    - Victoire occasionnelle : L'IA doit être capable de gagner parfois.
+    - Explication détaillée : Préparer une explication claire du fonctionnement de l'IA lors de l'évaluation.
+*/
+
+
+
 import { gameSettings } from './gameSettings.js';
 
 export function moveComputerPaddle(ball, paddleRight, canvas) {
---
+++ frontend/srcs/js/PongGame/power-ups.js
@@ -0,0 +1,26 @@
+// frontend/srcs/js/PongGame/power-ups.js
+
+/* 
+    idees de power-ups :
+
+    le power up sera a activer ou desactiver dans les parametres en debut de jeu
+    
+
+    Durée des effets : les powers ups vont durer entre 5 à 10 secondes
+    Apparition aléatoire : Les power-ups peuvent apparaître aléatoirement sur le terrain à intervalles réguliers
+    Indicateur visuel : Ajoute un indicateur visuel lorsque les power-ups sont actifs (couleur, taille, effet lumineux, etc.
+    
+    Idees de power-ups : 
+
+    Bonus :
+        - va augmenter temporairement la taille de notre paddle
+        - va augmenter temporairement la vitessse de notre paddle
+
+    Malus : 
+        - va reduire temporairement la taille de notre paddle
+        - va reduire temporairement la vitessse de notre paddle
+
+    Autre :
+        - ajoute une novuelle balle dans le jeu
+
+*/
