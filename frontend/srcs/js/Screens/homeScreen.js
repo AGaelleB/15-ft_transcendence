40998aad680cc8cad0f6c949404b4d88ff985a2b
Baptiste <b.fresquet06@gmail.com>
2024-10-02 15:15:34 +0200

    modal log out

--- frontend/srcs/js/Screens/homeScreen.js
+++ frontend/srcs/js/Screens/homeScreen.js
@@ -60,33 +60,41 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     // Obtenir les éléments pour le modal
+    const overlay = document.getElementById('overlay'); // Ajout de l'élément overlay
     const logoutLink = document.querySelector('.logout-link');
     const logoutModal = document.getElementById('logout-modal');
     const confirmLogoutButton = document.getElementById('confirm-logout');
     const cancelLogoutButton = document.getElementById('cancel-logout');
 
+    // Fonction pour ouvrir le modal et afficher l'overlay
+    function openModal() {
+        overlay.style.display = 'block';
+        logoutModal.style.display = 'flex'; // Ou 'block' selon ta préférence
+    }
+
+    // Fonction pour fermer le modal et cacher l'overlay
+    function closeModal() {
+        overlay.style.display = 'none';
+        logoutModal.style.display = 'none';
+    }
+
     // Lorsque l'icône de déconnexion est cliquée, afficher le modal
     logoutLink.addEventListener('click', function(event) {
         event.preventDefault(); // Empêcher la redirection immédiate
-        logoutModal.style.display = 'flex';
+        openModal(); // Appel de la fonction pour ouvrir le modal
     });
 
     // Si l'utilisateur confirme la déconnexion
     confirmLogoutButton.addEventListener('click', function() {
+        closeModal(); // Fermer le modal avant de rediriger
         window.location.href = "../index.html"; // Rediriger vers la page d'accueil
     });
 
     // Si l'utilisateur annule la déconnexion
-    cancelLogoutButton.addEventListener('click', function() {
-        logoutModal.style.display = 'none'; // Cacher le modal
-    });
-
+    cancelLogoutButton.addEventListener('click', closeModal);
+    
     // Si on clique en dehors du modal, on le ferme
-    window.addEventListener('click', function(event) {
-        if (event.target === logoutModal) {
-            logoutModal.style.display = 'none';
-        }
-    });
+    overlay.addEventListener('click', closeModal);
 
     updateSelection();
 });
