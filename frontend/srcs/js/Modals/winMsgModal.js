6f1cb7c7065e6b375117e8a0f684b05c9b201a98
AGaelleB <annegaelle.bonnefoy@gmail.com>
2024-10-07 15:39:13 +0200

    game pong 2d fix

--- frontend/srcs/js/Modals/winMsgModal.js
+++ frontend/srcs/js/Modals/winMsgModal.js
@@ -32,10 +32,14 @@ export function initializeWinMsg() {
             window.location.href = '1Player2D.html';
         else if (gameMode === '1 PLAYER 3D' || gameMode === '1 joueur 3D' || gameMode === '1 jugador 3D')
             window.location.href = '1Player3D.html';
-        else if (gameMode === '2 PLAYERS' || gameMode === '2 joueurs' || gameMode === '2 jugadores')
-            window.location.href = '2Players.html';
-        else if (gameMode === 'MULTI PLAYERS' || gameMode === 'Multijoueur' || gameMode === 'multijugadores')
-            window.location.href = 'multiPlayers.html';
+        else if (gameMode === '2 PLAYERS 2D' || gameMode === '2 joueurs 2D' || gameMode === '2 jugadores 2D')
+            window.location.href = '2Players2D.html';
+        else if (gameMode === '2 PLAYERS 3D' || gameMode === '2 joueurs 3D' || gameMode === '2 jugadores 3D')
+            window.location.href = '2Players3D.html';
+        else if (gameMode === 'MULTI PLAYERS 2D' || gameMode === 'Multijoueur 2D' || gameMode === 'multijugadores 2D')
+            window.location.href = 'multiPlayers2D.html';
+        else if (gameMode === 'MULTI PLAYERS 3D' || gameMode === 'Multijoueur 3D' || gameMode === 'multijugadores 3D')
+            window.location.href = 'multiPlayers3D.html';
         else
             console.error('Error: Mode de jeu non défini');
     });
