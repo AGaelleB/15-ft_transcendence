2f894fed3cec25b473a67a8fdf98f01788aa08f1
Bastien Guillaume <bguillau@student.42.fr>
2024-10-10 18:52:24 +0200

    clean + script for python venv+packages

---
+++ django/script.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+# USE IT IN TERMINAL WITH : source script.sh
+
+# if any python v env is already activated
+deactivate > /dev/null 2>&1 && echo "venv deactivated"
+
+# if any .venv already exists (cwd only)
+rm -rf .venv && echo ".venv removed"
+
+# create the virtual env (venv = command, .venv = repo, with .venv in .gitignore)
+python -m venv .venv && echo "venv .venv created"
+
+# activate it (should see (.venv) at very left of prompt)
+source .venv/bin/activate && echo ".venv activated"
+
+# install requierements for django
+echo "installing python packages"
+python -m pip install -r requirements.txt > /dev/null && echo "python packages installed"
+
+# set up database
+#python manage.py makemigrations
+#python migrate
+
+# launch server
+#python manage.py runserver
