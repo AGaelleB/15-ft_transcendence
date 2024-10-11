cb08c8faab3e203bd699f9c3149ef44f8cbad2b0
Battone1 <b.fresquet06@gmail.com>
2024-10-11 10:22:48 +0200

    sav fichiers html/templates nettoyés

---
+++ frontend/srcs/js/main.js
@@ -0,0 +1,15 @@
+// Définition des routes
+const routes = {
+    404: "../templates/404.html",
+    "/": "../templates/homeScreen.html",
+    "/start": "../templates/startScreen.html",
+    "/home": "../templates/homeScreen.html",
+    "/login": "../templates/loginSignUp.html",
+    "/profile": "../templates/userDashboard.html",
+    "/1player-2d": "../templates/1Player2D.html",
+    "/1player-3d": "../templates/1Player3D.html",
+    "/2players-2d": "../templates/2Players2D.html",
+    "/2players-3d": "../templates/2Players3D.html",
+    "/multi-2d": "../templates/multiPlayers2D.html",
+    "/multi-3d": "../templates/multiPlayers3D.html"
+};
