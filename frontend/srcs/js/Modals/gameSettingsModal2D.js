baa5de3d7a97bbd81a50aff7c4ec01581e6a38ec
Battone1 <b.fresquet06@gmail.com>
2024-10-22 16:31:41 +0200

    22/10/2024 16h 30

--- frontend/srcs/js/Modals/gameSettingsModal2D.js
+++ frontend/srcs/js/Modals/gameSettingsModal2D.js
@@ -130,7 +130,6 @@ export function updateSettingsModal2D() {
     updateSliderValuePosition('paddleSize', 'paddleSizeValue', 1, 16);
 }
 
-
 export function initializeGameSettings2D() {
     const settingsIcon = document.getElementById('settingsIcon');
     const settingsModal = document.getElementById('settingsModal');
@@ -150,7 +149,6 @@ export function initializeGameSettings2D() {
             settingsModal.style.display = 'none';
             saveGameSettings2D();
             loadGameSettings2D();
-            window.location.reload();
             isSettingsOpen2D = false;
         }
     });
@@ -160,8 +158,26 @@ export function initializeGameSettings2D() {
         settingsModal.style.display = 'none';
         saveGameSettings2D();
         loadGameSettings2D();
-        window.location.reload();
         isSettingsOpen2D = false;
+        const gameMode = localStorage.getItem('gameMode');
+        let targetPath = '/home';
+        if (gameMode === '1 PLAYER 2D' || gameMode === '1 joueur 2D' || gameMode === '1 jugador 2D')
+            targetPath = '/1player-2d';
+        else if (gameMode === '1 PLAYER 3D' || gameMode === '1 joueur 3D' || gameMode === '1 jugador 3D')
+            targetPath = '/1player-3d';
+        else if (gameMode === '2 PLAYERS 2D' || gameMode === '2 joueurs 2D' || gameMode === '2 jugadores 2D')
+            targetPath = '/2players-2d';
+        else if (gameMode === '2 PLAYERS 3D' || gameMode === '2 joueurs 3D' || gameMode === '2 jugadores 3D')
+            targetPath = '/2players-3d';
+        else if (gameMode === 'MULTI PLAYERS 2D' || gameMode === 'Multijoueur 2D' || gameMode === 'multijugadores 2D')
+            targetPath = '/multi-2d';
+        else if (gameMode === 'MULTI PLAYERS 3D' || gameMode === 'Multijoueur 3D' || gameMode === 'multijugadores 3D')
+            targetPath = '/multi-3d';
+        else
+            console.error('Error: Mode de jeu non défini');
+
+        window.history.pushState({}, "", targetPath);
+        handleLocation();
     });
     
     settingsModal.addEventListener('keydown', (e) => {
