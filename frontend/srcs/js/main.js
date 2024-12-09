13a4dee2bbcb9b2570b9fa13d45c12b00d4a4668
Baptiste <b.fresquet06@gmail.com>
2024-12-09 14:35:28 +0100

    404 OK

--- frontend/srcs/js/main.js
+++ frontend/srcs/js/main.js
@@ -22,29 +22,39 @@ const route = (event) => {
     handleLocation();
 };
 
+const isUserLoggedIn = () => {
+    const user = JSON.parse(localStorage.getItem('user'));
+    return !!user;
+};
+
+
 const handleLocation = async () => {
     let path = window.location.pathname;
     if (path.includes("/frontend/srcs/"))
         path = path.replace("/frontend/srcs", "");
-    
+
+    const publicRoutes = ['/login', '/start', '/index.html', '/'];
+    if (!isUserLoggedIn() && !publicRoutes.includes(path))
+        path = '/404'
+
     const route = routes[path] || routes[404];
     const html = await fetch(route).then((data) => data.text());
     document.getElementById("app").innerHTML = html;
 
+    if (path === '/404') {
+        const homeIcon404 = document.getElementById("homeIcon404");
+        if (homeIcon404) {
+            homeIcon404.addEventListener("click", () => {
+                window.history.pushState({}, "", "/start");
+                handleLocation();
+            });
+        }
+    }
+
     switch (path) {
         case '/start':
             import('./Screens/startScreen.js')
-                .then(module => {
-                    module.initializeStartScreen();
-                })
-                .catch(err => console.error('Failed to load startScreen.js:', err));
-            break;
-
-        case '/index.html':
-            import('./Screens/startScreen.js')
-                .then(module => {
-                    module.initializeStartScreen();
-                })
+                .then(module => module.initializeStartScreen())
                 .catch(err => console.error('Failed to load startScreen.js:', err));
             break;
 
@@ -60,75 +70,68 @@ const handleLocation = async () => {
                 })
                 .catch(err => console.error('Failed to load loginSignUp.js:', err));
             break;
+        
+        case '/index.html':
+            import('./Screens/startScreen.js')
+                .then(module => {
+                    module.initializeStartScreen();
+                })
+                .catch(err => console.error('Failed to load startScreen.js:', err));
+            break;
 
         case '/home':
             import('./Screens/homeScreen.js')
-            .then(module => {
-                module.initializeHome();
-            })
-            .catch(err => console.error('Failed to load homeScreen.js:', err));
+                .then(module => module.initializeHome())
+                .catch(err => console.error('Failed to load homeScreen.js:', err));
             break;
 
         case '/profil':
             import('./Screens/userDashboard.js')
-            .then(module => {
-                module.initializeProfil();
-            })
-            .catch(err => console.error('Failed to load userDashboard.js:', err));
+                .then(module => module.initializeProfil())
+                .catch(err => console.error('Failed to load userDashboard.js:', err));
             break;
 
         case '/1player-2d':
             import('./Screens/1Player2D.js')
-            .then(module => {
-                module.initialize1Player2D();
-            })
-            .catch(err => console.error('Failed to load 1Player2D.js:', err));
+                .then(module => module.initialize1Player2D())
+                .catch(err => console.error('Failed to load 1Player2D.js:', err));
             break;
 
         case '/1player-3d':
             import('./Screens/1Player3D.js')
-            .then(module => {
-                module.initialize1Player3D();
-            })
-            .catch(err => console.error('Failed to load 1Player3D.js:', err));
+                .then(module => module.initialize1Player3D())
+                .catch(err => console.error('Failed to load 1Player3D.js:', err));
             break;
 
         case '/2players-2d':
             import('./Screens/2Players2D.js')
-            .then(module => {
-                module.initialize2Players2D();
-            })
-            .catch(err => console.error('Failed to load 2Players2D.js:', err));
+                .then(module => module.initialize2Players2D())
+                .catch(err => console.error('Failed to load 2Players2D.js:', err));
             break;
 
         case '/2players-3d':
             import('./Screens/2Players3D.js')
-            .then(module => {
-                module.initialize2Players3D();
-            })
-            .catch(err => console.error('Failed to load 2Players3D.js:', err));
+                .then(module => module.initialize2Players3D())
+                .catch(err => console.error('Failed to load 2Players3D.js:', err));
             break;
 
         case '/tournament-2d':
             import('./Screens/tournament2D.js')
-            .then(module => {
-                module.initializeTournament2D();
-            })
-            .catch(err => console.error('Failed to load tournament2D.js:', err));
+                .then(module => module.initializeTournament2D())
+                .catch(err => console.error('Failed to load tournament2D.js:', err));
             break;
-    
+
         case '/tournament-3d':
             import('./Screens/tournament3D.js')
-            .then(module => {
-                module.initializeTournament3D();
-            })
-            .catch(err => console.error('Failed to load tournament3D.js:', err));
+                .then(module => module.initializeTournament3D())
+                .catch(err => console.error('Failed to load tournament3D.js:', err));
             break;
 
         default:
             break;
     }
 };
+    
 
 window.onpopstate = handleLocation;
 
