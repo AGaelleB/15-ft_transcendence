da407e6b488486cd1ff0073a4bfc8323e905ed1d
AGaelleB <annegaelle.bonnefoy@gmail.com>
2024-12-13 12:40:17 +0100

    config file in frontend for https

--- django/mysite/mysite/settings.py
+++ django/mysite/mysite/settings.py
@@ -18,8 +18,8 @@ ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 AUTH_USER_MODEL = 'base.User'
 
 CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:8000',
-    'http://localhost:8000'
+    'https://127.0.0.1:8000',
+    'https://localhost:8000'
 ]
 
 CORS_ALLOW_CREDENTIALS = True
